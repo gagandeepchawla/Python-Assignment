f27ebcf82a62624b70de74b167db9d72e6113b9c
gagandeepchawla <33547637+gagandeepchawla@users.noreply.github.com>
2017-11-14 14:38:02 +0530

    Delete alphabetic_order.py

--- alphabetic_order.py
+++
@@ -1,5 +0,0 @@
-#Write a Python Program to Sort Words in Alphabetic Order
-def alphabetic_0rder(ch):
-    return sorted(ch)
-letter = raw_input("Enter the letter\n")
-print (alphabetic_0rder(letter))
